1f829af2af690160e02a4ab790bcc050ee79955c
Jon Kuperman <jon.kuperman@gmail.com>
2015-12-08 21:44:45 -0800

    basic hapi app

---
+++ server.js
@@ -0,0 +1,16 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
