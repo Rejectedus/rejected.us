82bd17b8b3da6fc5e599c6a777cdee98a01cac36
Jon Kuperman <jon.kuperman@gmail.com>
2015-12-11 19:10:49 -0800

    react cleanup

--- views/default.jsx
+++ views/default.jsx
@@ -2,10 +2,42 @@ var React = require('react');
 
 var DefaultLayout = React.createClass({
   render: function() {
+    var githubStyle ={
+      position: 'fixed',
+      top: 0,
+      right: 0,
+      border: 0,
+      zIndex: 200
+    };
     return (
       <html>
-        <head><title>{this.props.title}</title></head>
-        <body>{this.props.children}</body>
+        <head>
+          <meta charset="utf-8"></meta>
+
+          <title>{this.props.title}</title>
+
+          <meta name="description" content="List of great programmers and companies they were rejected by."></meta>
+          <meta name="author" content="Jon Kuperman"></meta>
+          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+
+          <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"></link>
+          <link rel="stylesheet" type="text/css" href="/css/style.css"></link>
+        </head>
+        <body>
+          // Github ribbon
+          <a href="https://github.com/jkup/rejected.us">
+            <img style={githubStyle} src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></img>
+          </a>
+          // End Github ribbon
+          <header className="text-center">
+            <h1 className="title">They Rejected Us.</h1>
+            <div>Everyone’s been rejected - these are our stories</div>
+            <div>To add your own, please <a href="https://github.com/jkup/rejected.us/issues">open an issue</a> or tweet me at <a href="https://twitter.com/jkup">@jkup</a></div>
+          </header>
+          <div className="grid text-center Rejections">{this.props.children}</div>
+          <script src="/js/masonry.js"></script>
+          <script src="/js/script.js"></script>
+        </body>
       </html>
     );
   }
--- views/index.jsx
+++ views/index.jsx
@@ -2,13 +2,34 @@ var React = require('react');
 var DefaultLayout = require('./default');
 
 var Rejections = React.createClass({
-  render: function() {
-    var rows = [];
-    this.props.stories.forEach(function(story) {
-      rows.push(story.fullName);
+  componentDidMount: function() {
+    var msnry = new Masonry( '.grid', {
+      columnWidth: 200,
+      itemSelector: '.grid-item'
     });
+  },
+
+  render: function() {
+    var rejections = this.props.stories.map(function(story) {
+      return (
+        <div className="grid-item Rejection">
+          <div className="Rejection-Title">
+            <h3 className="Rejection-Title--FullName">
+              {story.fullName}
+              <span className="Rejection-Title--Handle">
+                <a target="_blank" href="https://twitter.com/getify">{story.handle}</a>
+              </span>
+            </h3>
+          </div>
+          <div className="Rejection-Bio">{story.bio}</div>
+          <img className="Rejection-Profile" alt="Jkup" src={"/img/" + story.avatar} />
+          <p>{story.story}</p>
+        </div>
+      )
+    });
+
     return (
-      <DefaultLayout>{rows}</DefaultLayout>
+      <DefaultLayout>{rejections}</DefaultLayout>
     )
   }
 });
