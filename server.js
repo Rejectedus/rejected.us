b6a58a08f00419b9f06a03c2f03318a30dcbd306
Jon Kuperman <jon.kuperman@gmail.com>
2015-12-16 14:35:12 -0800

    reverse instead of shuffle array #59

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 var express = require('express');
-var shuffle = require('shuffle-array')
 var app = express();
 
 // Public directory for all static assets
@@ -13,7 +12,7 @@ app.engine('jsx', require('express-react-views').createEngine());
 
 app.get('/', function(req, res) {
   const data = JSON.parse(fs.readFileSync('data/data.json', 'utf8'));
-  res.render('index', { title: "They Rejected Us.", stories: shuffle(data.stories) });
+  res.render('index', { title: "They Rejected Us.", stories: data.stories.reverse() });
 });
 
 var server = app.listen(8080, function () {
