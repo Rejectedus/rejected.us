fe1d87217704162b963276fc075fd8a4901356d7
Jon Kuperman <jon.kuperman@gmail.com>
2015-12-27 23:39:37 -0500

    remove localhost for production

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ function readData() {
   return JSON.parse(fs.readFileSync('data/data.json', 'utf8'));
 }
 
-var server = app.listen(8080, 'localhost', function () {
+var server = app.listen(8080, function () {
   console.log('Listening at http://localhost:8080');
 });
