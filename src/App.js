c938c6b58568a291772b773e9d17a4d6c4e9defa
Jon Kuperman <jon.kuperman@gmail.com>
2017-01-03 18:55:18 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Header from './Header'
-import './css/App.css';
+import './css/App.css'
 
 class App extends Component {
   render() {
     return (
-      <Header />
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+      <div>
+        <Header />
+        <div className="App">
+          <div className="App-header">
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './css/index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './css/index.css'
+import './css/bootstrap.min.css'
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+)
