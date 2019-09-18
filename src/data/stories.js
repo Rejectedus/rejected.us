922ac790730f8b9071a098b6b22fcb71902d57c8
Chad Ballay <chad.ballay@gmail.com>
2019-09-18 12:47:55 -0500

    Update stories.js

--- src/data/stories.js
+++ src/data/stories.js
@@ -811,7 +811,7 @@ module.exports = [
     bio: "Site Reliability Engineer, Walmart",
     avatar: "chadballay.jpg"
     story:
-      'Fresh out of college I interviewed and ultimately wasn\'t hired for position working in Walmart\'s call center.  The feedback I was given that they felt I wasn\'t suited for a tech support role.  After a year of trying a career change into teaching high school math, I reinterviewed and was hired as a developer.  Don\'t let a single interviewer\'s bad feedback mar your self confidence.',
+      'Fresh out of college, I interviewed and ultimately wasn\'t hired for position working in Walmart\'s call center.  The feedback I was given that they felt I wasn\'t suited for a tech support role.  After a year of trying a career change into teaching high school math, I reinterviewed and was hired as a developer.  Don\'t let a single interviewer\'s bad feedback mar your self confidence.',
     double: false
   }
 ];
